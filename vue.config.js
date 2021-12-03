37d57ab782b8c46304c6fc76cf15e98e36b0004d
weikaikai <1394987421@qq.com>
2021-12-03 18:11:46 +0800

    🔧 修改：新增actions等配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,11 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 // const CircularDependencyPlugin = require('circular-dependency-plugin')
 
+const isLib = process.env.TYPE === 'lib'
 module.exports = {
-  configureWebpack(config) {
-    console.log(config.plugins)
-  },
   chainWebpack(config) {
-    config.plugin('monaco').use(new MonacoWebpackPlugin())
-    // config.plugin('circular').use(new CircularDependencyPlugin())
+    if (!isLib) {
+      config.plugin('monaco').use(new MonacoWebpackPlugin())
+    }
   },
 }
